15099b8812447365edc9c16efcfb6b9a099b0ffe
AlicePacheco <AlicePacheco@30af6742-e830-11de-a198-37193048d1ed>
2011-12-03 15:09:10 +0000

    classe de utilidade geral no sistema.

---
+++ trunk/SimulacaoAD/src/br/ufrj/im/dcc/avaliacaodesempenho/utils/Funcoes.java
@@ -0,0 +1,39 @@
+package br.ufrj.im.dcc.avaliacaodesempenho.utils;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import br.ufrj.im.dcc.avaliacaodesempenho.estrutura.Bloco;
+
+/**
+ * Classe onde serão definidas as funções gerais utilizadas no sistema.
+ * 
+ * @version 1.0 
+ * */
+public class Funcoes {
+	
+	/* Funcao que gera um numero aleatorio uniformemente distribuido entre 0 e (i-1). */
+	public int geraUniforme(int i) {
+		Random aleatorio = new Random();
+		
+		return aleatorio.nextInt(i);
+	}
+	
+	/* 
+	 * Funcao que devolve o conjunto de blocos pertencentes ao peer de origem 
+	 * e nao pertencentes ao peer destino.
+	 * */
+	public ArrayList<Bloco> buscaBlocosNaoComuns(ArrayList<Bloco> peerOrigem, ArrayList<Bloco> peerDestino) {
+		ArrayList<Bloco> blocosNaoComuns = new ArrayList<Bloco>();
+		
+		for(Bloco bloco:peerOrigem){
+			if(!peerDestino.contains(bloco)) {
+				blocosNaoComuns.add(bloco);
+			}
+			
+		}
+		
+		return blocosNaoComuns;
+	}
+	
+}
