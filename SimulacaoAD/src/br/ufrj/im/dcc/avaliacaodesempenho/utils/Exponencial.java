1941ca69a0b73ef49f1e653a1b19a011260b747e
AlicePacheco <AlicePacheco@30af6742-e830-11de-a198-37193048d1ed>
2011-12-03 15:09:10 +0000

    classe de utilidade geral no sistema.

---
+++ SimulacaoAD/src/br/ufrj/im/dcc/avaliacaodesempenho/utils/Exponencial.java
@@ -0,0 +1,41 @@
+package br.ufrj.im.dcc.avaliacaodesempenho.utils;
+
+import java.util.Random;
+
+/**
+ * @version 1.0
+ * */
+public class Exponencial {
+	
+	private double taxa;
+	private Random numeroAleatorio;
+
+	public Exponencial(double taxa, long semente) {
+		
+		this.taxa = taxa;
+		
+		if(this.numeroAleatorio == null) {
+			this.numeroAleatorio = new Random(semente);
+		}
+	}
+	
+	/* Gerador de numero aleatorio exponencial. */
+	public double gerar() {
+		return -Math.log(1 - this.numeroAleatorio.nextDouble())/this.taxa;
+	}
+	
+	/* Calcula a esperanca matematica. */
+	public double getEsperanca() {
+		double esperanca = 1.0/this.taxa;
+		return esperanca;
+	}
+	
+	/* Calcula a variancia. */
+	public double getVariancia(){
+		double variancia = 1.0/(this.taxa * this.taxa);
+		return variancia;
+	}
+	
+	
+
+}
