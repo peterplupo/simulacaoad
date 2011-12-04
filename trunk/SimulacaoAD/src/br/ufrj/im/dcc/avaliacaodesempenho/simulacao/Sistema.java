75df6486eb108cc0a6da7ed31d7d9dadcf44450b
AlicePacheco <AlicePacheco@30af6742-e830-11de-a198-37193048d1ed>
2011-12-04 01:26:49 +0000

    modificacoes nos metodos de tratamento de eventos

--- trunk/SimulacaoAD/src/br/ufrj/im/dcc/avaliacaodesempenho/simulacao/Sistema.java
+++ trunk/SimulacaoAD/src/br/ufrj/im/dcc/avaliacaodesempenho/simulacao/Sistema.java
@@ -36,16 +36,14 @@ public class Sistema {
 	/**
 	 * Método para tratar evento do tipo Upload Publisher.
 	 * @param double 
-	 * @param Exponencial
 	 * */
-	protected void trataEventoUploadPublisher(double tempo, Exponencial uploadPublisher) {
+	protected void trataEventoUploadPublisher(double tempo) {
 		int qtdPeersSistema = peers.size();
-		double instanteUploadPublisher = uploadPublisher.gerar();
+
 		Evento evento = null;
-		
+		System.out.println("tempo publisher: " + publisher.getInstanteUploadPublisher());
 		if(qtdPeersSistema == 0) {
 			//seta novo tempo publisher
-			tempo = tempo + instanteUploadPublisher;    //time no qual ocorrerá o proximo upload do publisher.
 			publisher.setInstanteUploadPublisher(tempo);
 			
 			//adicionando evento Upload Publisher na lista de eventos.
@@ -70,9 +68,7 @@ public class Sistema {
 			}
 			
 			//seta novo tempo publisher
-			tempo = tempo + instanteUploadPublisher;    //time no qual ocorrerá o proximo upload do publisher.
 			publisher.setInstanteUploadPublisher(tempo);
-			
 			
 			//adicionando evento Upload Publisher na lista de eventos.
 			evento = new Evento(null, TiposEvento.UPLOAD_PUBLISHER, tempo);
@@ -84,10 +80,9 @@ public class Sistema {
 	 * Método para tratar evento do tipo Chegada Peer.
 	 * @param double 
 	 * @param double
-	 * @param Exponencial
-	 * @param Exponencial
+	 * @param double
 	 * */
-	protected void trataEventoChegadaPeer(double tempoEntrada, double tempo, Exponencial chegadaPeer, Exponencial uploadPeer) {
+	protected void trataEventoChegadaPeer(double tempoEntrada, double tempoChegadaPeer, double tempoUploadPeer) {
 		Evento evento = null;
 		
 		//insere peer no sistema.
@@ -96,25 +91,18 @@ public class Sistema {
 		peers.add(peer);
 		
 		if (sistemaAberto) {
-			//calcula tempo para a proxima chegada de peer no sistema.
-			double instanteChegadaSistema = chegadaPeer.gerar();
-			tempo = tempo + instanteChegadaSistema;
-			
 			//adicionando evento Chegada Peer na lista de eventos.
-			//evento = new Evento(peer, TiposEvento.CHEGADA_PEER, tempo);
-			evento = new Evento(null, TiposEvento.CHEGADA_PEER, tempo);
+			evento = new Evento(null, TiposEvento.CHEGADA_PEER, tempoChegadaPeer);
 			listaEventos.add(evento);
 			
 			
 		}
 		
-		//calcula tempo do upload desse peer.
-		double instanteUploadPeer = uploadPeer.gerar();
-		tempo = tempo + instanteUploadPeer;
-		peer.setInstanteUploadPeer(tempo);
+		//seta tempo do upload desse peer.
+		peer.setInstanteUploadPeer(tempoUploadPeer);
 		
 		//adicionando evento Upload Peer na lista de eventos.
-		evento = new Evento(peer, TiposEvento.UPLOAD_PEER, tempo);
+		evento = new Evento(peer, TiposEvento.UPLOAD_PEER, tempoUploadPeer);
 		listaEventos.add(evento);
 		
 		
@@ -123,11 +111,10 @@ public class Sistema {
 	/**
 	 * Método para tratar evento do tipo Upload Peer.
 	 * @param double 
-	 * @param Exponencial
 	 * @param Peer
-	 * @param Object
+	 * @param double
 	 * */
-	protected void trataEventoUploadPeer(double tempo, Exponencial uploadPeer, Peer peer, Object saidaPeer) {
+	protected void trataEventoUploadPeer(double tempoUploadPeer, Peer peer, double tempoSaidaPeer) {
 		Evento evento = null;
 		
 		//peer faz escolha de peer diferente de si proprio
@@ -153,12 +140,10 @@ public class Sistema {
 		}
 		
 		//seta tempo de proximo upload do peer escolhido
-		double instanteUploadPeer = uploadPeer.gerar();
-		tempo = tempo + instanteUploadPeer;
-		peers.get(peerEscolhido).setInstanteUploadPeer(tempo);
+		peers.get(peerEscolhido).setInstanteUploadPeer(tempoUploadPeer);
 		
 		//adicionando evento Upload Peer na lista de eventos.
-		evento = new Evento(peers.get(peerEscolhido), TiposEvento.UPLOAD_PEER, tempo);
+		evento = new Evento(peers.get(peerEscolhido), TiposEvento.UPLOAD_PEER, tempoUploadPeer);
 		listaEventos.add(evento);
 		
 		
@@ -167,20 +152,10 @@ public class Sistema {
 		int qtdBlocosPeerEscolhido = peers.get(peerEscolhido).getBlocosPeer().size();
 		if(qtdBlocosPublisher == qtdBlocosPeerEscolhido) {
 			System.out.println("passou em teste se qtd blocos no sistema e no peer ta ok.");
-			//boolean sistemaFechado = false;
-			if(saidaPeer instanceof Double) { //se ele sair imediatamente o tempo para saida eh 0.0
-				Double saida = (Double) saidaPeer;
-				tempo = tempo + saida;
-			} else {
-				Exponencial expSaida = (Exponencial) saidaPeer;
-				double saida = expSaida.gerar();
-				tempo = tempo + saida;
-			}
-			
-			peers.get(peerEscolhido).setInstanteSaidaSistema(tempo);
+			peers.get(peerEscolhido).setInstanteSaidaSistema(tempoSaidaPeer);
 
 			//adicionando evento Saida Peer na lista de eventos.
-			evento = new Evento(peers.get(peerEscolhido), TiposEvento.SAIDA_PEER, tempo);
+			evento = new Evento(peers.get(peerEscolhido), TiposEvento.SAIDA_PEER, tempoSaidaPeer);
 			listaEventos.add(evento);
 		}
 		
@@ -189,22 +164,19 @@ public class Sistema {
 	/**
 	 * Método para tratar evento do tipo Saida Peer.
 	 * @param double 
-	 * @param Exponencial
 	 * @param Peer
 	 * */
-	protected void trataEventoSaidaPeer(double tempo, Exponencial chegadaPeer, Peer peer) {
+	protected void trataEventoSaidaPeer(double tempoChegadaPeer, Peer peer) {
 		Evento evento = null;
 		
 		//peer sai do sistema
 		peers.remove(peer);
 		
 		if (!sistemaAberto) { 
-			//calcula tempo para proxima chegada de peer no sistema
-			double instanteChegadaSistema = chegadaPeer.gerar();
-			tempo = tempo + instanteChegadaSistema;
+			System.out.println("passou aqui - tratamento saida de peer");
 			
 			//adicionando evento Chegada Peer na lista de eventos.
-			evento = new Evento(null, TiposEvento.CHEGADA_PEER, tempo);
+			evento = new Evento(null, TiposEvento.CHEGADA_PEER, tempoChegadaPeer);
 			listaEventos.add(evento);
 		}
 		
